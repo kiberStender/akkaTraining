77f9f7915fe9fd7e314194f0ae374f42047f06ab
Kleber Stender <kleber.stender@gmail.com>
2016-09-25 11:12:38 -0300

    Created AkkademyDb actor

---
+++ akkademy-db/src/main/scala/com/akkademy/actors/AkkademyDb.scala
@@ -0,0 +1,23 @@
+package com.akkademy.actors
+
+import akka.actor.Actor
+import akka.event.Logging
+import com.akkademy.messages.SetRequest
+
+import scala.collection.mutable.HashMap
+
+/**
+  * Created by sirkleber on 25/09/16.
+  */
+class AkkademyDb extends Actor {
+  val map = new HashMap[String, Any]()
+  val log = Logging(context.system, this)
+
+  override def receive: Receive = {
+    case SetRequest(key, value) => {
+      log.info("received SetRequest - key: {} value: {}", key, value)
+      map.put(key, value)
+    }
+    case o: _ => log.info("received unknown message: {}", o)
+  }
+}
