260a6738deabc9ab09b322508ffdbdfad1e799a9
Kleber Stender <kleber.stender@gmail.com>
2016-09-25 11:20:08 -0300

    Created DbSpec

---
+++ akkademy-db/src/test/scala/org/akkademy/actorTest/AkkademyDbSpec.scala
@@ -0,0 +1,29 @@
+package org.akkademy.actorTest
+
+import akka.actor.ActorSystem
+import akka.testkit.TestActorRef
+import com.akkademy.actors.AkkademyDb
+import com.akkademy.messages.SetRequest
+import org.scalatest.{BeforeAndAfterEach, FunSpecLike, Matchers}
+
+/**
+  * Created by sirkleber on 25/09/16.
+  */
+class AkkademyDbSpec extends FunSpecLike with Matchers with BeforeAndAfterEach {
+  implicit val system = ActorSystem
+
+  describe("akkademyDb"){
+    describe("given SetRequest") {
+      it("sould place key/value into map"){
+        val actorRef = TestActorRef(new AkkademyDb)
+
+        actorRef ! SetRequest("name", "Kleber")
+
+        val akkademyDb = actorRef.underlyingActor
+
+        akkademyDb.map.get("name") should equal(Some("Kleber"))
+      }
+    }
+  }
+
+}
